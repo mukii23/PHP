ece705c8a6f8cbe713c2ed8f7e93fe33f06127c1
Mukii <mukii2307@gmail.com>
2018-11-13 12:18:58 +0530

    Create php-queries1.php
    Getting value from URL.

---
+++ php-queries1.php
@@ -0,0 +1,22 @@
+/**
+***Two ways to get the URL data***
+**/
+
+1. Suppose we have an URL - 'www.example.com/abc/xyz', we would get the 'abc' by using this method....
+
+    $url_path = trim(parse_url(add_query_arg(array()), PHP_URL_PATH), '/');
+    $explodeUrl = explode('/', $url_path);
+    $x = $explodeUrl[1];
+    
+2. Suppose we have an URL 'https://www.example.com/abc/xyz', we would get the 'xyz' by using this method....
+
+    $http = $_SERVER['HTTP_HOST'];
+    $url_paths = $_SERVER['REQUEST_URI'];
+    $whole_url = 'https://' . $http . $url_paths;
+    preg_match("/[^\/]+$/", $whole_url, $matches);
+    $last_word = $matches[0];
+    
+    
+/**
+***
+**/
