fe573e8f8b31c20d03d6208035cf1aca74badaf1
Mukii <mukii2307@gmail.com>
2022-07-13 16:21:10 +0530

    Create xampp-php-version.php

---
+++ xampp-php-version.php
@@ -0,0 +1,12 @@
+You can downgrade the PHP xampp version by using this:
+
+
+Download and extract the file xampp-win32-5.6.36-0-VC11.zip(eg. portable xampp version)
+
+Delete php folder and apache folder present in C:\xampp (currently installed)
+
+Copy php folder and apache folder from extracted file and paste it to C:\xampp
+
+Add " C: " before \xampp\ to php ini file present in php folder.
+
+Start your apache and MySQL and check php version. It will show php 5.6.36
